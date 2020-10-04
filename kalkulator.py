9914239b4095d57f8d71026c5f2dcfd90bce27ae
willn0rd <72337623+willn0rd@users.noreply.github.com>
2020-10-04 12:27:14 +0200

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,58 @@
+print("Witaj w kalkulatorze!")
+print("1 - dodawanie "
+      "2 - odejmowanie "
+      "3 - mnożenie "
+      "4 - dzielenie ")
+
+x = input()
+x = int(x)
+if x == 1:
+        print("pierwsza liczba: ")
+        y1 = input()
+        y1 = int(y1)
+
+        print("druga liczba: ")
+        y2 = input()
+        y2 = int(y2)
+
+        z = y1 + y2
+        print(y1," + ",y2," = ",z)
+
+elif x == 2:
+        print("pierwsza liczba: ")
+        y1 = input()
+        y1 = int(y1)
+
+        print("druga liczba: ")
+        y2 = input()
+        y2 = int(y2)
+
+        z = y1 - y2
+        print(y1, " - ", y2, " = ", z)
+
+elif x == 3:
+        print("pierwsza liczba: ")
+        y1 = input()
+        y1 = int(y1)
+
+        print("druga liczba: ")
+        y2 = input()
+        y2 = int(y2)
+
+        z = y1 * y2
+        print(y1, " x ", y2, " = ", z)
+
+elif x == 4:
+        print("pierwsza liczba: ")
+        y1 = input()
+        y1 = float(y1)
+
+        print("druga liczba: ")
+        y2 = input()
+        y2 = float(y2)
+
+        z = y1 / y2
+        print(y1, " : ", y2, " = ", z)
+
+else:
+    print("zła liczba!")
