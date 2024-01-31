814b05a2a8c904362f00df7ab561f456d63ccbe7
willn0rd <72337623+willn0rd@users.noreply.github.com>
2024-01-31 16:51:51 +0100

    Add files via upload

---
+++ python_nauka_1.py
@@ -0,0 +1,20 @@
+#print ("hello world!")
+#print ("Fajne to nawet")
+#print("mozna tez bez spacji?")
+#print("można")
+
+#znaczek # oznacza komentarz
+#print() -> funkcja w pythonie
+#"" -> string znaków
+
+print("hello world,\n" + "Fajne to nwawet,\n" + "mozna tez bez spacji \n")
+#lub 
+print(
+    """hello world,
+Fajne to nwawet,
+mozna tez bez spacji\n"""
+)
+
+#DODAJĄC JAKIKOLWIEK DRUGI OBIEKT DO FUNKCJI UŻYWA SIĘ ZNAKU + 
+
+print("proste, że można\n" * 5)
