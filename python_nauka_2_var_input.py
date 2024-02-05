42abeccc0b444e2b89d22317fe7b68226fa32b24
willn0rd <72337623+willn0rd@users.noreply.github.com>
2024-02-05 13:28:03 +0100

    Add files via upload

---
+++ python_nauka_2_var_input.py
@@ -0,0 +1,13 @@
+#robot Barista
+
+print("Hello, wlecome to coffee")
+
+#funkcja input() służy do wprowadzania wartości od użytkownika, automatycznie zamienia wartość na string
+#variable zapisuje wartość, można zapisać w dowolny sposób, przykładowo "name="
+name = input("What is your name? \n")
+
+print("Hello " + name + ", thanks for coming")
+
+menu = input("In Today's menu we have: \n" + "1. black coffe \n" + "2. flat white \n" + "3. white coffee \n" + "Which one would you like to drink? \n")
+
+print("Your " + menu + " will be ready in a couple moments")
